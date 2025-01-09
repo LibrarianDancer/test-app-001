3d70218e39279f1c7b02bed129a08c5a4a19077e
Aeon Kim <164200702+LibrarianDancer@users.noreply.github.com>
2025-01-09 12:43:42 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("😆 재미있는 스트림릿 웹앱 만들기")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
